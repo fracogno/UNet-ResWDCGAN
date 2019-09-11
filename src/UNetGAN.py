203d84722aef1b87bbc17a7844209531a0d97d8a
fracogno <francesco.cognolato@hotmail.com>
2019-09-11 12:05:07 +1000

    UNetGAN

---
+++ src/UNetGAN.py
@@ -0,0 +1,89 @@
+import tensorflow as tf
+
+
+def downsample(inputs, filters, size, isTraining, apply_batchnorm=True):
+    
+    result = tf.layers.conv2d(inputs, filters, size, strides=2, padding='SAME', use_bias=False, 
+                              kernel_initializer=tf.random_normal_initializer(0., 0.02))
+    
+    if apply_batchnorm:
+        result = tf.layers.batch_normalization(result, training=isTraining)
+
+    return tf.nn.leaky_relu(result)
+
+
+def upsample(inputs, filters, size, isTraining, apply_dropout=False):
+    
+    result = tf.layers.conv2d_transpose(inputs, filters, size, strides=2, padding='SAME', use_bias=False,
+                                        kernel_initializer=tf.random_normal_initializer(0., 0.02))
+    
+    result = tf.layers.batch_normalization(result, training=isTraining)
+
+    if apply_dropout:
+        result = tf.nn.dropout(result, keep_prob=0.5)
+
+    return tf.nn.relu(result)
+    
+    
+def getAutoencoder(X, isTraining):
+    filters = [32, 64, 128, 256, 512, 512, 512]
+    
+    with tf.variable_scope('autoencoder', reuse=tf.AUTO_REUSE):
+        output = X
+        print(output)
+        skips = []
+        # Encoder
+        for num_f in filters:
+            output = downsample(output, num_f, 5, isTraining, num_f != filters[0])
+            skips.append(output)
+            print(output)
+        
+        # Decoder
+        skips = reversed(skips[:-1])
+        for num_f, skip in zip(reversed(filters[:-1]), skips):
+            output = upsample(output, num_f, 5, isTraining, apply_dropout=num_f == 512)
+            output = tf.concat([output, skip], axis=3)
+            print(output)
+        
+        # Additional layers (output is bigger than input in this autoencoder)
+        output = upsample(output, 64, 5, isTraining, apply_dropout=False)
+        output = tf.concat([X, output], axis=3)
+        print(output)
+            
+        # Last layer => Only 3 channels and TANH
+        last = tf.layers.conv2d_transpose(output, 3, 5, strides=2, padding='SAME', activation="tanh",
+                                            kernel_initializer=tf.random_normal_initializer(0., 0.02))
+        print(last)    
+        print()
+
+        down1 = tf.image.resize_images(last, (128, 128))
+        print(down1)
+
+        print()
+        return last, down1
+
+
+def getAutoencoderDiscriminator(X, isTraining):
+
+    with tf.variable_scope('critic', reuse=tf.AUTO_REUSE):
+        initializer = tf.random_normal_initializer(0., 0.02)
+
+        down1 = downsample(X, 64, 4, isTraining, False)
+        down2 = downsample(down1, 128, 4, isTraining)
+        down3 = downsample(down2, 256, 4, isTraining)
+        print(X)
+        print(down1)
+        print(down2)
+        print(down3)
+
+        zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3) 
+        conv = tf.layers.conv2d(zero_pad1, 512, 4, strides=1, kernel_initializer=initializer, use_bias=False)
+        bn = tf.layers.batch_normalization(conv, training=True)
+        lrelu = tf.nn.leaky_relu(bn)
+        print(lrelu)
+
+        zero_pad2 = tf.keras.layers.ZeroPadding2D()(lrelu) 
+        last = tf.layers.conv2d(zero_pad2, 1, 4, strides=1, kernel_initializer=initializer)
+        print(last)
+        print()
+        return last
---
+++ trainUNetGAN.py
@@ -0,0 +1,89 @@
+import tensorflow as tf
+import src.util as util, src.ResWDCGAN as ResWDCGAN, src.UNetGAN as UNetGAN, src.losses as loss
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+basePath = "/scratch/cai/UNet-ResWDCGAN/"
+#basePath = "/home/francesco/UQ/UNet-ResWDCGAN/"
+
+import pickle
+with open(basePath + "dataset/TMP.pkl", 'rb') as handle:
+	X = pickle.load(handle)
+
+# Get data in different sizes
+imgSize = 256
+#X = util.getData(basePath + "dataset/SemArt/", ["semart_train.csv", "semart_val.csv", "semart_test.csv"], imgSize)
+print(X.shape)
+#assert(X.max() == 1. and X.min() == -1.)
+
+# Parameters
+epochsAE = 5000
+batchSize = 64
+lr = 1e-4
+Z_dim = 128
+l1_weight = 100.
+
+# Dataset
+def generator():
+	for el in X:
+		yield el
+
+dataset = tf.data.Dataset.from_generator(generator, (tf.float32),
+					output_shapes=(tf.TensorShape([imgSize, imgSize, 3]))).repeat(epochsAE).shuffle(buffer_size=len(X)).batch(batchSize, drop_remainder=True)
+iterator = dataset.make_initializable_iterator()
+X_tensor = iterator.get_next()
+
+Z = tf.placeholder(tf.float32, [None, Z_dim])
+isTraining = tf.placeholder(dtype=tf.bool)
+
+''' 
+	Networks
+'''
+G_z = ResWDCGAN.generator(Z, isTraining)
+G_AE, downsized = UNetGAN.getAutoencoder(G_z, isTraining)
+C_logits_real = UNetGAN.getAutoencoderDiscriminator(X_tensor, isTraining)
+C_logits_fake = UNetGAN.getAutoencoderDiscriminator(G_AE, isTraining)
+
+# Losses and optimizer
+C_loss, AE_gan = loss.GAN_Loss(C_logits_real, C_logits_fake, 0.9)
+AE_L1 = l1_weight * tf.reduce_mean(tf.abs(G_z - downsized)) 
+AE_loss = AE_gan + AE_L1
+
+C_optimizer, AE_optimizer = loss.Autoencoder_Optimizer(C_loss, AE_loss, lr, 0.5)
+
+# Tensorboard | VISUALIZE => tensorboard --logdir=.
+summaries_dir = basePath + "checkpoints"
+AE_summaries = [tf.summary.scalar('C_loss', C_loss), tf.summary.scalar('AE_L1', AE_L1), tf.summary.scalar('AE_gan', AE_gan)]
+AE_merged_summary = tf.summary.merge(AE_summaries)
+
+# Training
+with tf.Session(config=tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))) as sess:
+	saver = tf.train.Saver(max_to_keep=3000)
+	sess.run(tf.global_variables_initializer())
+
+	# Load pretrained generator 64x64
+	saverG = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='generator'))
+	saverG.restore(sess, basePath + "checkpoints/ckpt-G-78750")
+
+	AE_summary_writer = tf.summary.FileWriter(summaries_dir,  graph=tf.get_default_graph())
+
+	# Train AE GAN
+	AEStep = 0
+	sess.run(iterator.initializer)
+	try:
+		while True:
+			noise = util.sample_noise([batchSize, Z_dim])
+
+			# Train Critic
+			_, summary = sess.run([C_optimizer, AE_merged_summary], feed_dict={ isTraining: True, Z: noise })
+			AE_summary_writer.add_summary(summary, AEStep)
+			
+			# Train AE only
+			sess.run(AE_optimizer, feed_dict={ isTraining: True, Z: noise })
+
+			if AEStep % 750 == 0:
+				saver.save(sess, basePath + "checkpoints/ckpt-AE-" + str(AEStep))
+				AE_output = sess.run(G_AE, feed_dict={ isTraining : False, Z: util.sample_noise([4, Z_dim]) })
+				util.saveImages(basePath + "images/out-AE-" + str(AEStep), AE_output)
+			AEStep += 1
+	except tf.errors.OutOfRangeError:
+		pass
